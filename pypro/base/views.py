955365a912d6eb8f6890c3dc746a96c3e06eb632
Welinton <welinton@realizati.com.br>
2020-10-26 13:28:25 -0300

    Criado funções de assertions com base no TestCase do Django

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<html><body>Olá Django</body></html>')
+    return render(request, 'base/home.html')
