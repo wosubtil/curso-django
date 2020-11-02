9167c33f9f9522fad37a7ce328776b4cd3274dec
Welinton <welinton@realizati.com.br>
2020-11-02 15:52:47 -0300

    Adicionado novo video
    close #103

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 473382680}
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 473382680},
+        'instalacao-windows': {'titulo': 'Instalação Windows', 'vimeo_id': 474774894}
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
