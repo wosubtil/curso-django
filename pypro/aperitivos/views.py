4cb65abe839bd463d96a11a8dc1bf4be914388f0
Welinton <welinton@realizati.com.br>
2020-11-04 08:32:07 -0300

    Criado models do django para classe video
    close #122

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render
-from django.urls import reverse
 
-
-class Video:
-    def __init__(self, slug, titulo, vimeo_id):
-        self.titulo = titulo
-        self.slug = slug
-        self.vimeo_id = vimeo_id
-
-    def get_absolute_url(self):
-        return reverse('aperitivos:video', args=(self.slug,))
-
+from pypro.aperitivos.models import Video
 
 videos = [
-    Video('motivacao', 'Video Aperitivo: Motivação', 473382680),
-    Video('instalacao-windows', 'Instalação Windows', 474774894)
+    Video(slug='motivacao', titulo='Video Aperitivo: Motivação', vimeo_id='473382680'),
+    Video(slug='instalacao-windows', titulo='Instalação Windows', vimeo_id='474774894')
 ]
 
 videos_dct = {v.slug: v for v in videos}
