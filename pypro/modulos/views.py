33266e281208e5541f1953d3ff14867e03c2a879
Welinton <welinton@realizati.com.br>
2020-11-25 08:51:42 -0300

    Finalização da criação do setor Módulos
    close #148

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from pypro.modulos import facade
+
+
 def detalhe(request, slug):
-    pass
+    modulo = facade.encontrar_modulo(slug)
+    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo})
