067b1c4379703b60493ac9f35067d46d03ea912d
Welinton <welinton@realizati.com.br>
2020-11-25 08:17:19 -0300

    Adição do Slug para o Modulos - Lógica nas migrações

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from ordered_model.models import OrderedModel
 
 
@@ -6,9 +7,13 @@ class Modulo(OrderedModel):
     titulo = models.CharField(max_length=64)
     publico = models.TextField(null=True)
     descricao = models.TextField(null=True)
+    slug = models.SlugField(unique=True)
 
     class Meta(OrderedModel.Meta):
         pass
 
     def __str__(self):
         return self.titulo
+
+    def get_absulute_url(self):
+        return reverse('modulos:detalhes', kwargs={'slug': self.slug})
